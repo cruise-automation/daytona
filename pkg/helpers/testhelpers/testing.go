e3fb96e906851560de838131bbf18efc2c7f92e9
Ryan Boehning <ryanboehning@gmail.com>
2019-06-03 13:40:18 -0700

    Fix lint issues caught by golangci-lint
    Add missing error checks.
    Deindent package comments.
    Remove unnecessary type casts.

--- pkg/helpers/testhelpers/testing.go
+++ pkg/helpers/testhelpers/testing.go
@@ -5,7 +5,11 @@ import "github.com/hashicorp/vault/api"
 func GetTestClient(url string) (*api.Client, error) {
 	vaultConfig := api.DefaultConfig()
 	vaultConfig.Address = url
-	vaultConfig.ConfigureTLS(&api.TLSConfig{Insecure: true})
+	err := vaultConfig.ConfigureTLS(&api.TLSConfig{Insecure: true})
+	if err != nil {
+		return nil, err
+	}
+
 	client, err := api.NewClient(vaultConfig)
 	if err != nil {
 		return nil, err
