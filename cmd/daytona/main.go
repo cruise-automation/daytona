4699c0d24b0fc55d226e16b6695d6da8efa28d45
Arnaud Farbos <arnaud.farbos@getcruise.com>
2020-08-27 14:55:11 -0700

    Create a function to run daytona

--- cmd/daytona/main.go
+++ cmd/daytona/main.go
@@ -19,19 +19,9 @@ package main
 import (
 	"flag"
 	"log"
-	"os"
-	"os/exec"
-	"os/signal"
-	"path/filepath"
-	"strconv"
-	"syscall"
 
-	"github.com/cruise-automation/daytona/pkg/auth"
 	cfg "github.com/cruise-automation/daytona/pkg/config"
-	"github.com/cruise-automation/daytona/pkg/pki"
-	"github.com/cruise-automation/daytona/pkg/secrets"
-	"github.com/hashicorp/vault/api"
-	homedir "github.com/mitchellh/go-homedir"
+	"github.com/cruise-automation/daytona/pkg/daytona"
 )
 
 var config cfg.Config
@@ -39,103 +29,39 @@ var config cfg.Config
 // this is populated at build time
 var version string
 
-const (
-	flagAWSIAMAuth = "aws-auth"
-	flagK8SAuth    = "k8s-auth"
-	flagGCPAuth    = "gcp-auth"
-)
-
 func init() {
-	flag.StringVar(&config.VaultAddress, "address", "", "Sets the vault server address. The default vault address or VAULT_ADDR environment variable is used if this is not supplied")
-	flag.StringVar(&config.TokenPath, "token-path", cfg.BuildDefaultConfigItem("TOKEN_PATH", "~/.vault-token"), "a full file path where a token will be read from/written to (env: TOKEN_PATH)")
-	flag.BoolVar(&config.K8SAuth, flagK8SAuth, func() bool {
-		b, err := strconv.ParseBool(cfg.BuildDefaultConfigItem("K8S_AUTH", "false"))
-		return err == nil && b
-	}(), "select kubernetes vault auth as the vault authentication mechanism (env: K8S_AUTH)")
-	flag.BoolVar(&config.AWSAuth, flagAWSIAMAuth, func() bool {
-		b, err := strconv.ParseBool(cfg.BuildDefaultConfigItem("IAM_AUTH", "false"))
-		return err == nil && b
-	}(), "select AWS IAM vault auth as the vault authentication mechanism (env: IAM_AUTH)")
-	flag.BoolVar(&config.AWSAuth, "iam-auth", func() bool {
-		b, err := strconv.ParseBool(cfg.BuildDefaultConfigItem("IAM_AUTH", "false"))
-		return err == nil && b
-	}(), "(legacy) select AWS IAM vault auth as the vault authentication mechanism (env: IAM_AUTH)")
-	flag.StringVar(&config.K8STokenPath, "k8s-token-path", cfg.BuildDefaultConfigItem("K8S_TOKEN_PATH", "/var/run/secrets/kubernetes.io/serviceaccount/token"), "kubernetes service account jtw token path (env: K8S_TOKEN_PATH)")
-	flag.StringVar(&config.VaultAuthRoleName, "vault-auth-role", cfg.BuildDefaultConfigItem("VAULT_AUTH_ROLE", ""), "the name of the role used for auth. used with either auth method (env: VAULT_AUTH_ROLE, note: will infer to k8s sa account name if left blank)")
-	flag.BoolVar(&config.GCPAuth, flagGCPAuth, func() bool {
-		b, err := strconv.ParseBool(cfg.BuildDefaultConfigItem("GCP_AUTH", "false"))
-		return err == nil && b
-	}(), "select Google Cloud Platform IAM auth as the vault authentication mechanism (env: GCP_AUTH)")
-	flag.StringVar(&config.GCPServiceAccount, "gcp-svc-acct", cfg.BuildDefaultConfigItem("GCP_SVC_ACCT", ""), "the name of the service account authenticating (env: GCP_SVC_ACCT)")
-	flag.Int64Var(&config.RenewalInterval, "renewal-interval", func() int64 {
-		b, err := strconv.ParseInt(cfg.BuildDefaultConfigItem("RENEWAL_INTERVAL", "300"), 10, 64)
-		if err != nil {
-			return 900
-		}
-		return b
-	}(), "how often to check the token's ttl and potentially renew it (env: RENEWAL_INTERVAL)")
-	flag.Int64Var(&config.RenewalThreshold, "renewal-threshold", func() int64 {
-		b, err := strconv.ParseInt(cfg.BuildDefaultConfigItem("RENEWAL_THRESHOLD", "7200"), 10, 64)
-		if err != nil {
-			return 7200
-		}
-		return b
-	}(), "the threshold remaining in the vault token, in seconds, after which it should be renewed (env: RENEWAL_THRESHOLD)")
-	flag.Int64Var(&config.RenewalIncrement, "renewal-increment", func() int64 {
-		b, err := strconv.ParseInt(cfg.BuildDefaultConfigItem("RENEWAL_INCREMENT", "43200"), 10, 64)
-		if err != nil {
-			return 43200
-		}
-		return b
-	}(), "the value, in seconds, to which the token's ttl should be renewed (env: RENEWAL_INCREMENT)")
-	flag.StringVar(&config.SecretPayloadPath, "secret-path", cfg.BuildDefaultConfigItem("SECRET_PATH", ""), "the full file path to store the JSON blob of the fetched secrets (env: SECRET_PATH)")
-	flag.BoolVar(&config.AutoRenew, "auto-renew", func() bool {
-		b, err := strconv.ParseBool(cfg.BuildDefaultConfigItem("AUTO_RENEW", "false"))
-		return err == nil && b
-	}(), "if enabled, starts the token renewal service (env: AUTO_RENEW)")
-	flag.BoolVar(&config.Entrypoint, "entrypoint", func() bool {
-		b, err := strconv.ParseBool(cfg.BuildDefaultConfigItem("ENTRYPOINT", "false"))
-		return err == nil && b
-	}(), "if enabled, execs the command after the separator (--) when done. mostly useful with -secret-env (env: ENTRYPOINT)")
-	flag.BoolVar(&config.SecretEnv, "secret-env", func() bool {
-		b, err := strconv.ParseBool(cfg.BuildDefaultConfigItem("SECRET_ENV", "false"))
-		return err == nil && b
-	}(), "write secrets to environment variables (env: SECRET_ENV)")
-	flag.BoolVar(&config.InfiniteAuth, "infinite-auth", func() bool {
-		b, err := strconv.ParseBool(cfg.BuildDefaultConfigItem("INFINITE_AUTH", "false"))
-		return err == nil && b
-	}(), "infinitely attempt to authenticate (env: INFINITE_AUTH)")
-	flag.Int64Var(&config.MaximumAuthRetry, "max-auth-duration", func() int64 {
-		b, err := strconv.ParseInt(cfg.BuildDefaultConfigItem("MAX_AUTH_DURATION", "300"), 10, 64)
-		if err != nil {
-			return 300
-		}
-		return b
-	}(), "the value, in seconds, for which DAYTONA should attempt to renew a token before exiting (env: MAX_AUTH_DURATION)")
-	flag.StringVar(&config.K8SAuthMount, "k8s-auth-mount", cfg.BuildDefaultConfigItem("K8S_AUTH_MOUNT", "kubernetes"), "the vault mount where k8s auth takes place (env: K8S_AUTH_MOUNT, note: will infer via k8s metadata api if left unset)")
-	flag.StringVar(&config.AWSAuthMount, "iam-auth-mount", cfg.BuildDefaultConfigItem("IAM_AUTH_MOUNT", "aws"), "the vault mount where iam auth takes place (env: IAM_AUTH_MOUNT)")
-	flag.StringVar(&config.GCPAuthMount, "gcp-auth-mount", cfg.BuildDefaultConfigItem("GCP_AUTH_MOUNT", "gcp"), "the vault mount where gcp auth takes place (env: GCP_AUTH_MOUNT)")
-	flag.StringVar(&config.AuthMount, "auth-mount", cfg.BuildDefaultConfigItem("AUTH_MOUNT", ""), "")
-	flag.IntVar(&config.Workers, "workers", func() int {
-		b, err := strconv.ParseInt(cfg.BuildDefaultConfigItem("WORKERS", "1"), 10, 64)
-		if err != nil {
-			log.Fatal("WORKERS environment variable must be a valid value")
-		}
+	config = cfg.DefaultConfig()
 
-		if b < 1 || b > 5 {
-			log.Fatal("-workers must be greater than zero and less than 5")
-		}
-		return int(b)
-	}(), "how many workers to run to read secrets in parallel (env: WORKERS) (Max: 5)")
-	flag.StringVar(&config.PkiIssuer, "pki-issuer", cfg.BuildDefaultConfigItem("PKI_ISSUER", ""), "the name of the PKI CA backend to use when requesting a certificate (env: PKI_ISSUER)")
-	flag.StringVar(&config.PkiRole, "pki-role", cfg.BuildDefaultConfigItem("PKI_ROLE", ""), "the name of the PKI role to use when requesting a certificate (env: PKI_ROLE)")
-	flag.StringVar(&config.PkiDomains, "pki-domains", cfg.BuildDefaultConfigItem("PKI_DOMAINS", ""), "a comma-separated list of domain names to use when requesting a certificate (env: PKI_DOMAINS)")
-	flag.StringVar(&config.PkiPrivateKey, "pki-privkey", cfg.BuildDefaultConfigItem("PKI_PRIVKEY", ""), "a full file path where the vault-issued private key will be written to (env: PKI_PRIVKEY)")
-	flag.StringVar(&config.PkiCertificate, "pki-cert", cfg.BuildDefaultConfigItem("PKI_CERT", ""), "a full file path where the vault-issued x509 certificate will be written to (env: PKI_CERT)")
-	flag.BoolVar(&config.PkiUseCaChain, "pki-use-ca-chain", func() bool {
-		b, err := strconv.ParseBool(cfg.BuildDefaultConfigItem("PKI_USE_CA_CHAIN", "false"))
-		return err == nil && b
-	}(), "if set, retrieve the CA chain and include it in the certificate file output (env: PKI_USE_CA_CHAIN)")
+	flag.StringVar(&config.VaultAddress, "address", "", "Sets the vault server address. The default vault address or VAULT_ADDR environment variable is used if this is not supplied")
+	flag.StringVar(&config.TokenPath, "token-path", config.TokenPath, "a full file path where a token will be read from/written to (env: TOKEN_PATH)")
+	flag.BoolVar(&config.K8SAuth, cfg.FlagK8SAuth, config.K8SAuth, "select kubernetes vault auth as the vault authentication mechanism (env: K8S_AUTH)")
+	flag.BoolVar(&config.AWSAuth, cfg.FlagAWSIAMAuth, config.AWSAuth, "select AWS IAM vault auth as the vault authentication mechanism (env: IAM_AUTH)")
+	flag.BoolVar(&config.AWSAuth, "iam-auth", config.AWSAuth, "(legacy) select AWS IAM vault auth as the vault authentication mechanism (env: IAM_AUTH)")
+	flag.StringVar(&config.K8STokenPath, "k8s-token-path", config.K8STokenPath, "kubernetes service account jtw token path (env: K8S_TOKEN_PATH)")
+	flag.StringVar(&config.VaultAuthRoleName, "vault-auth-role", config.VaultAuthRoleName, "the name of the role used for auth. used with either auth method (env: VAULT_AUTH_ROLE, note: will infer to k8s sa account name if left blank)")
+	flag.BoolVar(&config.GCPAuth, cfg.FlagGCPAuth, config.GCPAuth, "select Google Cloud Platform IAM auth as the vault authentication mechanism (env: GCP_AUTH)")
+	flag.StringVar(&config.GCPServiceAccount, "gcp-svc-acct", config.GCPServiceAccount, "the name of the service account authenticating (env: GCP_SVC_ACCT)")
+	flag.Int64Var(&config.RenewalInterval, "renewal-interval", config.RenewalInterval, "how often to check the token's ttl and potentially renew it (env: RENEWAL_INTERVAL)")
+	flag.Int64Var(&config.RenewalThreshold, "renewal-threshold", config.RenewalThreshold, "the threshold remaining in the vault token, in seconds, after which it should be renewed (env: RENEWAL_THRESHOLD)")
+	flag.Int64Var(&config.RenewalIncrement, "renewal-increment", config.RenewalIncrement, "the value, in seconds, to which the token's ttl should be renewed (env: RENEWAL_INCREMENT)")
+	flag.StringVar(&config.SecretPayloadPath, "secret-path", config.SecretPayloadPath, "the full file path to store the JSON blob of the fetched secrets (env: SECRET_PATH)")
+	flag.BoolVar(&config.AutoRenew, "auto-renew", config.AutoRenew, "if enabled, starts the token renewal service (env: AUTO_RENEW)")
+	flag.BoolVar(&config.Entrypoint, "entrypoint", config.Entrypoint, "if enabled, execs the command after the separator (--) when done. mostly useful with -secret-env (env: ENTRYPOINT)")
+	flag.BoolVar(&config.SecretEnv, "secret-env", config.SecretEnv, "write secrets to environment variables (env: SECRET_ENV)")
+	flag.BoolVar(&config.InfiniteAuth, "infinite-auth", config.InfiniteAuth, "infinitely attempt to authenticate (env: INFINITE_AUTH)")
+	flag.Int64Var(&config.MaximumAuthRetry, "max-auth-duration", config.MaximumAuthRetry, "the value, in seconds, for which DAYTONA should attempt to renew a token before exiting (env: MAX_AUTH_DURATION)")
+	flag.StringVar(&config.K8SAuthMount, "k8s-auth-mount", config.K8SAuthMount, "the vault mount where k8s auth takes place (env: K8S_AUTH_MOUNT, note: will infer via k8s metadata api if left unset)")
+	flag.StringVar(&config.AWSAuthMount, "iam-auth-mount", config.AWSAuthMount, "the vault mount where iam auth takes place (env: IAM_AUTH_MOUNT)")
+	flag.StringVar(&config.GCPAuthMount, "gcp-auth-mount", config.GCPAuthMount, "the vault mount where gcp auth takes place (env: GCP_AUTH_MOUNT)")
+	flag.StringVar(&config.AuthMount, "auth-mount", config.AuthMount, "")
+	flag.IntVar(&config.Workers, "workers", config.Workers, "how many workers to run to read secrets in parallel (env: WORKERS) (Max: 5)")
+	flag.StringVar(&config.PkiIssuer, "pki-issuer", config.PkiIssuer, "the name of the PKI CA backend to use when requesting a certificate (env: PKI_ISSUER)")
+	flag.StringVar(&config.PkiRole, "pki-role", config.PkiRole, "the name of the PKI role to use when requesting a certificate (env: PKI_ROLE)")
+	flag.StringVar(&config.PkiDomains, "pki-domains", config.PkiDomains, "a comma-separated list of domain names to use when requesting a certificate (env: PKI_DOMAINS)")
+	flag.StringVar(&config.PkiPrivateKey, "pki-privkey", config.PkiPrivateKey, "a full file path where the vault-issued private key will be written to (env: PKI_PRIVKEY)")
+	flag.StringVar(&config.PkiCertificate, "pki-cert", config.PkiCertificate, "a full file path where the vault-issued x509 certificate will be written to (env: PKI_CERT)")
+	flag.BoolVar(&config.PkiUseCaChain, "pki-use-ca-chain", config.PkiUseCaChain, "if set, retrieve the CA chain and include it in the certificate file output (env: PKI_USE_CA_CHAIN)")
+
 }
 
 func main() {
@@ -143,83 +69,7 @@ func main() {
 	log.Printf("Starting %s...\n", version)
 	flag.Parse()
 
-	if !config.ValidateAuthType() {
-		log.Fatalf("You must provide an auth method: -%s or -%s or -%s\n", flagK8SAuth, flagAWSIAMAuth, flagGCPAuth)
-	}
-
-	switch {
-	case config.K8SAuth:
-		auth.InferK8SConfig(&config)
-		config.BuildAuthMountPath(config.K8SAuthMount)
-	case config.AWSAuth:
-		config.BuildAuthMountPath(config.AWSAuthMount)
-	case config.GCPAuth:
-		config.BuildAuthMountPath(config.GCPAuthMount)
-	}
-
-	if err := config.ValidateConfig(); err != nil {
+	if err := daytona.Run(config, flag.Args()...); err != nil {
 		log.Fatalln(err.Error())
-	}
-
-	fullTokenPath, err := homedir.Expand(config.TokenPath)
-	if err != nil {
-		log.Println("Could not expand", config.TokenPath, "using it as-is")
-	} else {
-		config.TokenPath = fullTokenPath
-	}
-	if f, err := os.Stat(config.TokenPath); err == nil && f.IsDir() {
-		log.Println("The provided token path is a directory, automatically appending .vault-token filename")
-		config.TokenPath = filepath.Join(config.TokenPath, ".vault-token")
-	}
-
-	vaultConfig := api.DefaultConfig()
-	if config.VaultAddress != "" {
-		vaultConfig.Address = config.VaultAddress
-	}
-	client, err := api.NewClient(vaultConfig)
-	if err != nil {
-		log.Fatalf("Could not configure vault client. error: %s\n", err)
-	}
-
-	if !auth.EnsureAuthenticated(client, config) {
-		log.Fatalln("The maximum elapsed time has been reached for authentication attempts. exiting.")
-	}
-
-	secrets.SecretFetcher(client, config)
-	pki.CertFetcher(client, config)
-
-	if config.AutoRenew {
-		// if you send USR1, we'll re-fetch secrets
-		sigChan := make(chan os.Signal, 1)
-		signal.Notify(sigChan,
-			syscall.SIGUSR1)
-
-		go func() {
-			for {
-				s := <-sigChan
-				switch s {
-				case syscall.SIGUSR1:
-					secrets.SecretFetcher(client, config)
-					pki.CertFetcher(client, config)
-				}
-			}
-		}()
-		auth.RenewService(client, config)
-	}
-
-	if config.Entrypoint {
-		args := flag.Args()
-		if len(args) == 0 {
-			log.Fatalln("No arguments detected with use of -entrypoint")
-		}
-		log.Println("Will exec: ", args)
-		binary, err := exec.LookPath(args[0])
-		if err != nil {
-			log.Fatalf("Error finding '%s' to exec: %s\n", args[0], err)
-		}
-		err = syscall.Exec(binary, args, os.Environ())
-		if err != nil {
-			log.Fatalf("Error from exec: %s\n", err)
-		}
 	}
 }
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,12 +1,27 @@
 package config
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
+	"path/filepath"
+	"strconv"
+
+	"cloud.google.com/go/compute/metadata"
+	"github.com/briankassouf/jose/jws"
+	homedir "github.com/mitchellh/go-homedir"
 )
 
-const authPathFmtString = "auth/%s/login"
+const (
+	FlagAWSIAMAuth = "aws-auth"
+	FlagK8SAuth    = "k8s-auth"
+	FlagGCPAuth    = "gcp-auth"
+
+	authPathFmtString = "auth/%s/login"
+)
 
 //Config represents an application configurations
 type Config struct {
@@ -40,13 +55,92 @@ type Config struct {
 	PkiUseCaChain     bool
 }
 
-// BuildDefaultConfigItem uses the following operation: ENV --> arg
-func BuildDefaultConfigItem(envKey string, def string) (val string) {
+// buildDefaultConfigItem uses the following operation: ENV --> arg
+func buildDefaultConfigItem(envKey string, def string) (val string) {
 	val = os.Getenv(envKey)
 	if val == "" {
 		val = def
 	}
 	return
+}
+
+func DefaultConfig() Config {
+	var (
+		err     error
+		workers int64
+		config  = Config{
+			AWSAuthMount:      buildDefaultConfigItem("IAM_AUTH_MOUNT", "aws"),
+			AuthMount:         buildDefaultConfigItem("AUTH_MOUNT", ""),
+			GCPAuthMount:      buildDefaultConfigItem("GCP_AUTH_MOUNT", "gcp"),
+			GCPServiceAccount: buildDefaultConfigItem("GCP_SVC_ACCT", ""),
+			K8SAuthMount:      buildDefaultConfigItem("K8S_AUTH_MOUNT", "kubernetes"),
+			K8STokenPath:      buildDefaultConfigItem("K8S_TOKEN_PATH", "/var/run/secrets/kubernetes.io/serviceaccount/token"),
+			PkiCertificate:    buildDefaultConfigItem("PKI_CERT", ""),
+			PkiDomains:        buildDefaultConfigItem("PKI_DOMAINS", ""),
+			PkiIssuer:         buildDefaultConfigItem("PKI_ISSUER", ""),
+			PkiPrivateKey:     buildDefaultConfigItem("PKI_PRIVKEY", ""),
+			PkiRole:           buildDefaultConfigItem("PKI_ROLE", ""),
+			SecretPayloadPath: buildDefaultConfigItem("SECRET_PATH", ""),
+			TokenPath:         buildDefaultConfigItem("TOKEN_PATH", "~/.vault-token"),
+			VaultAuthRoleName: buildDefaultConfigItem("VAULT_AUTH_ROLE", ""),
+		}
+	)
+
+	if workers, err = strconv.ParseInt(buildDefaultConfigItem("WORKERS", "1"), 10, 64); err == nil {
+		config.Workers = int(workers)
+	} else {
+		log.Fatal("WORKERS environment variable must be a valid value")
+	}
+
+	if config.AutoRenew, err = strconv.ParseBool(buildDefaultConfigItem("AUTO_RENEW", "false")); err != nil {
+		config.AutoRenew = false
+	}
+
+	if config.Entrypoint, err = strconv.ParseBool(buildDefaultConfigItem("ENTRYPOINT", "false")); err != nil {
+		config.Entrypoint = false
+	}
+
+	if config.GCPAuth, err = strconv.ParseBool(buildDefaultConfigItem("GCP_AUTH", "false")); err != nil {
+		config.GCPAuth = false
+	}
+
+	if config.AWSAuth, err = strconv.ParseBool(buildDefaultConfigItem("IAM_AUTH", "false")); err != nil {
+		config.AWSAuth = false
+	}
+
+	if config.InfiniteAuth, err = strconv.ParseBool(buildDefaultConfigItem("INFINITE_AUTH", "false")); err != nil {
+		config.InfiniteAuth = false
+	}
+
+	if config.MaximumAuthRetry, err = strconv.ParseInt(buildDefaultConfigItem("MAX_AUTH_DURATION", "300"), 10, 64); err != nil {
+		config.MaximumAuthRetry = 300
+	}
+
+	if config.PkiUseCaChain, err = strconv.ParseBool(buildDefaultConfigItem("PKI_USE_CA_CHAIN", "false")); err != nil {
+		config.PkiUseCaChain = false
+	}
+
+	if config.RenewalIncrement, err = strconv.ParseInt(buildDefaultConfigItem("RENEWAL_INCREMENT", "43200"), 10, 64); err != nil {
+		config.RenewalIncrement = 43200
+	}
+
+	if config.RenewalInterval, err = strconv.ParseInt(buildDefaultConfigItem("RENEWAL_INTERVAL", "300"), 10, 64); err != nil {
+		config.RenewalInterval = 900
+	}
+
+	if config.RenewalThreshold, err = strconv.ParseInt(buildDefaultConfigItem("RENEWAL_THRESHOLD", "7200"), 10, 64); err != nil {
+		config.RenewalThreshold = 7200
+	}
+
+	if config.SecretEnv, err = strconv.ParseBool(buildDefaultConfigItem("SECRET_ENV", "false")); err != nil {
+		config.SecretEnv = false
+	}
+
+	if config.K8SAuth, err = strconv.ParseBool(buildDefaultConfigItem("K8S_AUTH", "false")); err != nil {
+		config.K8SAuth = false
+	}
+
+	return config
 }
 
 // ValidateAuthType validates that the user has supplied
@@ -81,10 +175,100 @@ func (c *Config) ValidateConfig() error {
 	return nil
 }
 
-// BuildAuthMountPath attempts to construct a mount path
-// if the provided one is empty
-func (c *Config) BuildAuthMountPath(path string) {
-	if c.AuthMount == "" {
-		c.AuthMount = fmt.Sprintf(authPathFmtString, path)
+// inferK8SConfig attempts to replace default configuration parameters on K8S with ones inferred from the k8s environment
+func (c *Config) inferK8SConfig() {
+	log.Println("Attempting to automatically infer some k8s configuration data")
+	if c.VaultAuthRoleName == "" {
+		saName, err := c.inferVaultAuthRoleName()
+		if err != nil {
+			log.Printf("Unable to infer SA Name: %v\n", err)
+		} else {
+			c.VaultAuthRoleName = saName
+		}
 	}
+
+	// Check for default value
+	if c.K8SAuthMount == "kubernetes" {
+		vaultAuthMount, err := inferVaultAuthMount()
+		if err != nil {
+			log.Printf("Unable to infer K8S Vault Auth Mount: %v\n", err)
+		} else {
+			c.K8SAuthMount = vaultAuthMount
+		}
+	}
+}
+
+// inferVaultAuthRoleName figures out the current k8s service account name, and returns it.
+// This can be assumed to match a a vault role if configured properly.
+func (c *Config) inferVaultAuthRoleName() (string, error) {
+	data, err := ioutil.ReadFile(c.K8STokenPath)
+	if err != nil {
+		return "", fmt.Errorf("could not read JWT from file %s", err.Error())
+	}
+
+	jwt := bytes.TrimSpace(data)
+
+	// Parse into JWT
+	parsedJWT, err := jws.ParseJWT(jwt)
+	if err != nil {
+		return "", err
+	}
+
+	saName, ok := parsedJWT.Claims().Get("kubernetes.io/serviceaccount/service-account.name").(string)
+	if !ok || saName == "" {
+		return "", errors.New("could not parse UID from claims")
+	}
+
+	return saName, nil
+}
+
+// inferVaultAuthMount attempts to figure out where the auth path for the current k8s cluster is in vault, and return it
+func inferVaultAuthMount() (string, error) {
+	clusterName, err := metadata.InstanceAttributeValue("cluster-name")
+
+	return fmt.Sprintf("kubernetes-gcp-%s", clusterName), err
+}
+
+// buildAuthMountPath attempts to construct a mount path
+// if the provided one is empty
+func (c *Config) buildAuthMountPath() {
+	switch {
+	case c.K8SAuth:
+		c.inferK8SConfig()
+		c.AuthMount = fmt.Sprintf(authPathFmtString, c.K8SAuthMount)
+	case c.AWSAuth:
+		c.AuthMount = fmt.Sprintf(authPathFmtString, c.AWSAuthMount)
+	case c.GCPAuth:
+		c.AuthMount = fmt.Sprintf(authPathFmtString, c.GCPAuthMount)
+	}
+}
+
+func (c *Config) ValidateAndBuild() error {
+	if c.Workers < 1 || c.Workers > 5 {
+		return errors.New("-workers must be greater than zero and less than 5")
+	}
+
+	if !c.ValidateAuthType() {
+		return fmt.Errorf("You must provide an auth method: -%s or -%s or -%s\n", FlagK8SAuth, FlagAWSIAMAuth, FlagGCPAuth)
+	}
+
+	c.buildAuthMountPath()
+
+	if err := c.ValidateConfig(); err != nil {
+		return err
+	}
+
+	fullTokenPath, err := homedir.Expand(c.TokenPath)
+	if err != nil {
+		log.Println("Could not expand", c.TokenPath, "using it as-is")
+	} else {
+		c.TokenPath = fullTokenPath
+	}
+
+	if f, err := os.Stat(c.TokenPath); err == nil && f.IsDir() {
+		log.Println("The provided token path is a directory, automatically appending .vault-token filename")
+		c.TokenPath = filepath.Join(c.TokenPath, ".vault-token")
+	}
+
+	return nil
 }
