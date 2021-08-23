749aa12977e4fcb6d717e65f789bbd3424e96c8b
Nathan Button <nathan.button@getcruise.com>
2021-08-23 15:16:03 +0000

    Add JWT method

--- pkg/auth/azure.go
+++ pkg/auth/azure.go
@@ -47,12 +47,35 @@ type simplifiedCompute struct {
 	SubscriptionID    string `json:"subscriptionId"`
 }
 
-func (a *AWSService) getJWT()(string, error) {
+type simplifiedToken struct {
+	AccessToken string `json:"access_token"`
+}
+
+func (a *AWSService) getJWT(baseURL string) (string, error) {
+	// TODO: build the request in parts
 	// response=$(curl 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F' -H Metadata:true -s)
-	jwt 
+	jwtEndpoint := fmt.Sprintf("%s/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://management.azure.com/", baseURL)
+	r, err := http.NewRequest(http.MethodGet, jwtEndpoint, nil)
+	if err != nil {
+		return "", err
+	}
+
+	r.Header.Add("Metadata", "true")
+
+	resp, err := http.DefaultClient.Do(r)
+	if err != nil {
+		return "", err
+	}
+
+	token := new(simplifiedToken)
+	err = json.NewDecoder(resp.Body).Decode(token)
+
+	return token.AccessToken, err
+
 }
 
 func (a *AWSService) getMetadata(baseURL string) (*simplifiedMetadata, error) {
+	// TODO: build the request in parts
 	// Get metadata
 	// Metadata is not exposed through the Azure-sdk-for-go https://github.com/Azure/azure-sdk-for-go/issues/982
 	// metadata=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance?api-version=2017-08-01")
@@ -62,7 +85,7 @@ func (a *AWSService) getMetadata(baseURL string) (*simplifiedMetadata, error) {
 		return nil, err
 	}
 
-	r.Header.Add("Metadata","true")
+	r.Header.Add("Metadata", "true")
 
 	resp, err := http.DefaultClient.Do(r)
 	if err != nil {
@@ -72,5 +95,5 @@ func (a *AWSService) getMetadata(baseURL string) (*simplifiedMetadata, error) {
 	metadata := new(simplifiedMetadata)
 	err = json.NewDecoder(resp.Body).Decode(metadata)
 
-	return metadata, err 
+	return metadata, err
 }
